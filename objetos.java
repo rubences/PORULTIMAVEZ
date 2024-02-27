abef5d3ef0e5766791052f89383c91348c8bdec3
rubences <91731294+rubences@users.noreply.github.com>
2024-02-27 13:07:36 +0000

    cambios

--- objetos.java
+++ objetos.java
@@ -1,37 +1,51 @@
 public class Objetos {
     public static class Vector3D {
-        int x, y, z;
+        private int x, y, z;
+
+        public Vector3D(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector3D sum(Vector3D other) {
+            int newX = this.x + other.x;
+            int newY = this.y + other.y;
+            int newZ = this.z + other.z;
+            return new Vector3D(newX, newY, newZ);
+        }
+
+        public Vector3D crossProduct(Vector3D other) {
+            int newX = this.y * other.z - this.z * other.y;
+            int newY = this.x * other.z - this.z * other.x;
+            int newZ = this.x * other.y - this.y * other.x;
+            return new Vector3D(newX, newY, newZ);
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public int getZ() {
+            return z;
+        }
     }
 
     public static void main(String[] args) {
-        Vector3D v1 = new Vector3D();
-        Vector3D v2 = new Vector3D();
-        Vector3D v3 = new Vector3D();
-        Vector3D vs = new Vector3D();
-        Vector3D vp = new Vector3D();
-
-        v1.x = 1;
-        v1.y = 0;
-        v1.z = 0;
-
-        v2.x = 0;
-        v2.y = 1;
-        v2.z = 0;
-
-        v3.x = 0;
-        v3.y = 0;
-        v3.z = 1;
+        Vector3D v1 = new Vector3D(1, 0, 0);
+        Vector3D v2 = new Vector3D(0, 1, 0);
+        Vector3D v3 = new Vector3D(0, 0, 1);
 
         // Suma
-        vs.x = v1.x + v2.x;
-        vs.y = v1.y + v2.y;
-        vs.z = v1.z + v2.z;
-        System.out.printf("La suma es %d, %d, %d\n\n", vs.x, vs.y, vs.z);
+        Vector3D vs = v1.sum(v2);
+        System.out.printf("La suma es %d, %d, %d\n\n", vs.getX(), vs.getY(), vs.getZ());
 
         // Producto
-        vp.x = vs.y * v3.z - vs.z * v3.y;
-        vp.y = vs.x * v3.z - vs.z * v3.x;
-        vp.z = vs.x * v3.y - vs.y * v3.x;
-        System.out.printf("El producto es %d, %d, %d\n\n", vp.x, vp.y, vp.z);
+        Vector3D vp = vs.crossProduct(v3);
+        System.out.printf("El producto es %d, %d, %d\n\n", vp.getX(), vp.getY(), vp.getZ());
     }
 }
