4178f6c31f8fc98158d55d5506b08a3e1c5254ee
rubences <91731294+rubences@users.noreply.github.com>
2024-02-28 10:56:29 +0000

    ahora si que la hemos liado

---
+++ casito_nulito/src/main/java/ejercicitos_supensitos_toditos/App.java
@@ -0,0 +1,13 @@
+package ejercicitos_supensitos_toditos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
